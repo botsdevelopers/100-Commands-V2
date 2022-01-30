abb33b32cbcf63dc01ef3414909b3aba6dd08682
TEDDY <75193966+TEDDY6969@users.noreply.github.com>
2022-01-30 23:08:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.PREFIX = "!";
-exports.OWNER_ID = "";
-exports.Owner_Name = "DarkThunder#1899";
+exports.PREFIX = "&";// bot prefix
+exports.OWNER_ID = ("890146474158075915", "796006192970334228");//your id
+exports.Owner_Name = "SUNSHİNE || Halloweenjesse";// Your username
+const mySecret = process.env['TOKEN']
